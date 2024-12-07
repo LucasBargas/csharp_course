661c892093b995cbd2e6eec2edd35e754258def9
Lucas Bargas <lucas.bargasoficial@gmail.com>
2024-12-07 12:00:46 -0300

    first commit

---
+++ 1_curso_csharp_orientado_a_objetos/md1_primeira_aplicacao/aula_1_criando_ambiente/Program.cs
@@ -0,0 +1,10 @@
+﻿// See Screen Sound
+
+int age = 24;
+string welcome = "Boas vindas ao Screen Sound";
+string nome = "Lucas";
+string sobrenome = "Bargas";
+// int outline = 33;
+char letter = 'L';
+
+Console.WriteLine($"Olá, {nome} {sobrenome}! {welcome}! Você tem {age} anos, correto? Seu nome começa com a letra '{letter}.'");
